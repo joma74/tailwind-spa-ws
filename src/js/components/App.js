ede65b5d7d324adc2381d3c920d56f22bb448e34
joma74 <dev-mails@gmx.net>
2018-04-20 23:20:12 +0200

    feat: replaced body with app.html's root element

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -3,4 +3,7 @@ import renderFnkt from "@home/app.html?style=@home/app.css";
 
 /* eslint-disable no-new */
 export default renderFnkt({
+  mounted() {
+    document.documentElement.replaceChild(this.$el, document.body);
+  }
 });
