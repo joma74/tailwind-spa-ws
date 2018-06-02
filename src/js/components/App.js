d1980656c59582bfa62f55b63d2b661da748d9ef
joma74 <dev-mails@gmx.net>
2018-06-02 23:24:41 +0200

    chore(format): result of run js_frmt

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,6 +1,5 @@
 // @ts-ignore
-import renderFnkt from "@home/app.html?style=@home/app.css";
-
+import renderFnkt from "@home/app.html?style=@home/app.css"
 
 /** @type {import("vue").ComponentOptions} */
 let vueComponentOptions = {
@@ -8,12 +7,16 @@ let vueComponentOptions = {
   mounted() {
     /** @type {import("vue").default} */
     // @ts-ignore
-    const compInst = this;
-    if(compInst.$el instanceof HTMLBodyElement)
-      document.documentElement.replaceChild(compInst.$el, document.body);
+    const compInst = this
+    if (compInst.$el instanceof HTMLBodyElement)
+      document.documentElement.replaceChild(compInst.$el, document.body)
     else
-      console.error(`this.el having a value of >>${compInst.$el}<< should be an HTMLBodyElement but is not`);
-  }
-};
+      console.error(
+        `this.el having a value of >>${
+          compInst.$el
+        }<< should be an HTMLBodyElement but is not`,
+      )
+  },
+}
 
-export default renderFnkt(vueComponentOptions);
+export default renderFnkt(vueComponentOptions)
