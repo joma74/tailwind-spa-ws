6f4890c8593a014054e943233bb2ba43d52203ce
joma74 <dev-mails@gmx.net>
2018-08-21 01:10:42 +0200

    test: Check expected elements

---
+++ src/tests/test.js
@@ -0,0 +1,27 @@
+import { Selector } from "testcafe"
+
+fixture("Getting Started").page("http://localhost:8080/dist/index.html")
+
+test("Check expected elements", async (t) => {
+  const heading = await Selector("h1[data-desc='heading']")
+
+  let headingText = await heading.innerText
+
+  await t.expect(headingText).eql("Greetings hot-updateable tailwindcss")
+
+  const svgDashboard = await Selector("svg[data-desc='dashboard']")
+
+  await t.expect(svgDashboard.visible).ok()
+
+  const svgCog = await Selector("svg[data-desc='cog']")
+
+  await t.expect(svgCog.visible).ok()
+
+  const imageWebpack = await Selector("img[data-desc='webpack']")
+
+  await t.expect(imageWebpack.visible).ok()
+
+  const imageVue = await Selector("img[data-desc='vue']")
+
+  await t.expect(imageVue.visible).ok()
+})
