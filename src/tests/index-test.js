d9fb64e0653a552f299eede816f325a4ebcfdbe1
joma74 <dev-mails@gmx.net>
2018-08-24 00:50:13 +0200

    feat(test): check innerHtml of subheadline which including the svgs

--- src/tests/index-test.js
+++ src/tests/index-test.js
@@ -1,4 +1,5 @@
-import { Selector } from "testcafe"
+import { Selector, ClientFunction } from "testcafe"
+import pretty from "pretty"
 
 fixture("Index Page Test").page("http://localhost:8080/dist/index.html")
 
@@ -12,6 +13,23 @@ test("dom has critical elements", async (t) => {
   const svgDashboard = await Selector("svg[data-desc='dashboard']")
 
   await t.expect(svgDashboard.visible).ok()
+
+  const subheadline = await Selector("p[data-desc='subheadline']")
+
+  const subheadlineValue = await t.eval(() => subheadline().innerHTML, {
+    dependencies: { subheadline },
+  })
+
+  const expectedSubHeadlineValue = pretty(`
+  	<svg data-desc="dashboard" class="w-6 h-6 opacity-50 fill-current relative mr-2">
+  		<use href="src/assets/svg/toolbar.svg#dashboard"></use>
+	</svg>
+	Powered by
+	<svg data-desc="cog" class="w-6 h-6 opacity-50 fill-current relative ml-2">
+		<use href="src/assets/svg/toolbar.svg#cog"></use>
+	</svg>`)
+
+  await t.expect(pretty(subheadlineValue)).eql(expectedSubHeadlineValue)
 
   const svgCog = await Selector("svg[data-desc='cog']")
 
