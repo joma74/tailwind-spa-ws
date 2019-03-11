27a6064a0123ec1eebef74a41eff3c4d5dba8fd8
joma74 <dev-mails@gmx.net>
2019-03-11 17:25:54 +0100

    chore: merge bp over from brrww-1

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,8 @@ const path = require("path")
 const webpack = require("webpack")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 const ExtractTextPlugin = require("extract-text-webpack-plugin")
-var DiskPlugin = require("webpack-disk-plugin")
+const HardSourceWebpackPlugin = require("hard-source-webpack-plugin")
+const DiskPlugin = require("webpack-disk-plugin")
 const prettyFormat = require("pretty-format")
 
 /**
@@ -22,9 +23,87 @@ const webpack_aliase = {
   "@font": path.resolve(__dirname, "src/assets/fonts/"),
   "@home": path.resolve(__dirname),
   "@html": path.resolve(__dirname, "src/html/"),
+  "@icons": path.resolve(__dirname, "src/assets/icons/"),
   "@img": path.resolve(__dirname, "src/assets/img/"),
   "@js": path.resolve(__dirname, "src/js/"),
   "@svg": path.resolve(__dirname, "src/assets/svg/"),
+}
+
+/**
+ * @type {import("webpack").Rule[]}
+ */
+const rules = [
+  {
+    test: /\.html$/,
+    exclude: [/src\/template.html/],
+    use: {
+      loader: "vue-template-loader",
+      options: {
+        hmr: true,
+        transformAssetUrls: {
+          video: ["src", "poster"],
+          source: "src",
+          img: "src",
+          image: "xlink:href",
+          object: "data",
+          use: ["href", "xlink:href"],
+        },
+      },
+    },
+  },
+  {
+    test: /\.css$/,
+    use: [
+      "style-loader",
+      {
+        loader: "css-loader",
+        options: {
+          importLoaders: 1,
+        },
+      },
+      "postcss-loader",
+    ],
+  },
+  {
+    test: /\.(png|jpe?g|gif|svg)(#.*)?(\?.*)?$/,
+    use: [
+      {
+        loader: "file-loader",
+        options: {
+          limit: 10000,
+          emitFile: true,
+          name: "[name].[ext]",
+          useRelativePath: true,
+        },
+      },
+    ],
+  },
+  {
+    test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+    loader: "url-loader",
+    options: {
+      limit: 10000,
+      emitFile: true,
+      name: "[name].[ext]",
+      useRelativePath: true,
+    },
+  },
+]
+
+/**
+ * @type {import("webpack").Node}
+ */
+const node = {
+  // prevent webpack from injecting useless setImmediate polyfill because Vue
+  // source contains it (although only uses it if it's native).
+  setImmediate: false,
+  // prevent webpack from injecting mocks to Node native modules
+  // that does not make sense for the client
+  dgram: "empty",
+  fs: "empty",
+  net: "empty",
+  tls: "empty",
+  child_process: "empty",
 }
 
 /**
@@ -35,7 +114,7 @@ var webpackConfig = [
     entry: "./src/js/index",
     output: {
       path: path.resolve(__dirname, "dist"),
-      filename: "bundle.js",
+      filename: "[name].js",
     },
     devServer,
     resolve: {
@@ -46,82 +125,39 @@ var webpackConfig = [
     },
     // devtool: "source-map",
     module: {
-      rules: [
-        {
-          test: /\.html$/,
-          exclude: [/src\/template.html/],
-          use: {
-            loader: "vue-template-loader",
-            options: {
-              hmr: true,
-              transformAssetUrls: {
-                video: ["src", "poster"],
-                source: "src",
-                img: "src",
-                image: "xlink:href",
-                object: "data",
-                use: ["href", "xlink:href"],
-              },
-            },
-          },
-        },
-        {
-          test: /\.css$/,
-          use: [
-            "style-loader",
-            {
-              loader: "css-loader",
-              options: {
-                importLoaders: 1,
-              },
-            },
-            "postcss-loader",
-          ],
-        },
-        {
-          test: /\.(png|jpe?g|gif|svg)(#.*)?(\?.*)?$/,
-          use: [
-            {
-              loader: "file-loader",
-              options: {
-                limit: 10000,
-                emitFile: true,
-                name: "[name].[ext]",
-                useRelativePath: true,
-              },
-            },
-          ],
-        },
-        {
-          test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-          loader: "url-loader",
-          options: {
-            limit: 10000,
-            emitFile: true,
-            name: "[name].[ext]",
-            useRelativePath: true,
-          },
-        },
-      ],
+      rules,
     },
-    node: {
-      // prevent webpack from injecting useless setImmediate polyfill because Vue
-      // source contains it (although only uses it if it's native).
-      setImmediate: false,
-      // prevent webpack from injecting mocks to Node native modules
-      // that does not make sense for the client
-      dgram: "empty",
-      fs: "empty",
-      net: "empty",
-      tls: "empty",
-      child_process: "empty",
-    },
+    node,
     plugins: [
+      new webpack.optimize.CommonsChunkPlugin({
+        name: "vendor",
+        minChunks: function(module) {
+          return module.context && module.context.indexOf("node_modules") !== -1
+        },
+      }),
       new HtmlWebpackPlugin({
         template: "src/template.html",
         title: "Template",
       }),
       new webpack.NamedModulesPlugin(),
+      new HardSourceWebpackPlugin({
+        // Clean up large, old caches automatically.
+        cachePrune: {
+          // Caches younger than `maxAge` are not considered for deletion. They must
+          // be at least this (default: 2 days) old in milliseconds.
+          maxAge: 2 * 24 * 60 * 60 * 1000,
+          // All caches together must be larger than `sizeThreshold` before any
+          // caches will be deleted. Together they must be at least this
+          // (default: 10 MB) big in bytes.
+          sizeThreshold: 10 * 1024 * 1024,
+        },
+        info: {
+          // 'debug', 'log', 'info', 'warn', or 'error'.
+          level: "debug",
+          // 'none' or 'test'.
+          mode: "none",
+        },
+      }),
     ],
   },
   {
@@ -136,37 +172,14 @@ var webpackConfig = [
       },
     },
     module: {
-      rules: [
-        {
-          test: /\.css$/,
-          use: ExtractTextPlugin.extract({
-            use: [
-              {
-                loader: "css-loader",
-                options: {
-                  importLoaders: 1,
-                },
-              },
-              "postcss-loader",
-            ],
-          }),
-        },
-        {
-          test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-          loader: "url-loader",
-          options: {
-            limit: 10000,
-            emitFile: true,
-            name: "[name].[ext]",
-            useRelativePath: true,
-          },
-        },
-      ],
+      rules,
     },
+    node,
     plugins: [
       new webpack.NamedModulesPlugin(),
       new ExtractTextPlugin("styles.css"),
       // Write out asset files to disk.
+      // @ts-ignore
       new DiskPlugin({
         output: {
           path: path.resolve(__dirname, "build"),
